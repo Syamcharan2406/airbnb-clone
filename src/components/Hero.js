30f60b27ce3e4432ad8292d89175113bab5624fc
Syamcharan2406 <144098199+Syamcharan2406@users.noreply.github.com>
2024-04-23 18:53:52 +0530

    navbar and hero

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HeroImg from '../images/photo-grid.png';
+
+function Hero(){
+    return(
+        <div className="hero">
+            <img className="hero-img" src={HeroImg} alt="a photo grid" />
+            <h1 className="hero-header">Online Experince</h1>
+            <p className="hero-text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    );
+}
+
+export default Hero;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/airbnb-logo.png';
+
+function Navbar(){
+    return (
+        <div className='nav'>
+            <img className='nav-logo' src= {logo} alt='Airbnb logo'/>
+        </div>
+    );
+}
+
+export default Navbar;
